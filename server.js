8c647782d3fbc269cd16c226ee1c5c8f1d481047
caleb-melchizedek <codeprogrammer25112018@gmail.com>
2022-02-02 02:20:08 +0000

    addressing server issue

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
-app.set('view engine', 'ejs')
 const port = normalizePort(process.env.PORT || '3000');
+
+var http = require('http');
+
+app.set('view engine', 'ejs');
+app.set('port', port);
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -27,4 +31,57 @@ io.on('connection', socket => {
   })
 })
 
+
+
 server.listen(port)
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
